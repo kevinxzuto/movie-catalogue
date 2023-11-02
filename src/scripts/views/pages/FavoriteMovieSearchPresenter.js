a64b81e7c08ca0526d726fb179ce5b3427042f49
kevinxzuto <4521210025@univpancasila.ac.id>
2023-11-02 20:19:07 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/views/pages/FavoriteMovieSearchPresenter.js
@@ -0,0 +1,47 @@
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies }) {
+    this._listenToSearchRequestByUser();
+    this._favoriteMovies = favoriteMovies;
+  }
+
+  _listenToSearchRequestByUser() {
+    this._queryElement = document.getElementById('query');
+    this._queryElement.addEventListener('change', (event) => {
+      this._searchMovies(event.target.value);
+    });
+  }
+
+  async _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery.trim();
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+    this._showFoundMovies(foundMovies);
+  }
+
+  _showFoundMovies(movies) {
+    console.log(movies);
+    const html = movies.reduce(
+      (carry, movie) => carry.concat(`
+        <li class="movie">
+          <span class="movie__title">${movie.title || '-'}</span>
+        </li>
+      `),
+      '',
+    );
+    document.querySelector('.movies').innerHTML = html;
+
+    document
+      .getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'));
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteMovieSearchPresenter;
